7144753f4e08c4cc70ab783286990b84129620be
gabrielemuci98 <muci.gabriele@iisgalvanimi.edu.it>
2019-10-08 10:48:19 +0000

    esercizio finito

--- EXPRESS_STATIC_FILES/app.js
+++ EXPRESS_STATIC_FILES/app.js
@@ -6,6 +6,14 @@ app.get('/',function(req,res){
  res.sendFile(path.join(__dirname+'/index.html')); //__dirname : Ritorna la cartella del progetto
 });
 
+app.get('/about',function(req,res){
+ res.sendFile(path.join(__dirname+'/about.html')); //__dirname : Ritorna la cartella del progetto
+});
+
+app.get('/sitemap',function(req,res){
+ res.sendFile(path.join(__dirname+'/sitemap.html')); //__dirname : Ritorna la cartella del progetto
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
